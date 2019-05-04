5f26510fc2debbb4c374b061cb84ae381e740246
Grzegorz <grzegorz.wilczynski@student.put.poznan.pl>
2019-05-04 15:21:25 +0200

    Utworzenie szkieletu bazy danych

--- DataBaseModel.py
+++ DataBaseModel.py
@@ -19,22 +19,25 @@ alerts_people = Table('alerts_people', Base.metadata,
 
 class Person(Base):
     __tablename__ = 'People'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
-    birth_date = Column(Date(), nullable=False)
+    birth_date = Column(Date(), nullable=True)
     alerts = relationship("Alert", secondary=alerts_people, back_populates="persons")
 
 
-class HonoraryMember(Base):
+class HonoraryMember(Person):
     __tablename__ = 'HonoraryMembers'
+
     is_active = Column(Boolean, nullable=False)
     person_id = Column(Integer, ForeignKey('People.id'), primary_key=True)
     person = relationship(Person)
 
 
-class Firefighter(Base):
+class Firefighter(Person):
     __tablename__ = 'Firefighters'
+
     is_active = Column(Boolean, nullable=False)
     person_id = Column(Integer, ForeignKey('People.id'), primary_key=True)
     person = relationship(Person)
@@ -42,21 +45,23 @@ class Firefighter(Base):
 
 class Alert(Base):
     __tablename__ = 'Alerts'
+
     id = Column(Integer, primary_key=True)
     timestamp = Column(DateTime, nullable=False)
     reason = Column(String(500), nullable=False)
-    persons = relationship("Person", secondary=alerts_people, back_populates="alerts_persons")
-    vehicles = relationship("Vehicle", secondary=alerts_vehicles, back_populates="alerts_vehicles")
+    persons = relationship("Person", secondary=alerts_people, back_populates="alerts")
+    vehicles = relationship("Vehicle", secondary=alerts_vehicles, back_populates="alerts")
 
 
 class Vehicle(Base):
     __tablename__ = 'Vehicles'
+
     id = Column(Integer, primary_key=True)
     type = Column(String(500), nullable=False)
     name = Column(String(500), nullable=False)
     description = Column(String(500), nullable=False)
     seats_amount = Column(Integer, nullable=False)
-    alerts = relationship("Alert", secondary=alerts_vehicles, back_populates="alerts")
+    alerts = relationship("Alert", secondary=alerts_vehicles, back_populates="vehicles")
 
 
 # Create an engine that stores data in the local directory's
---
+++ Iserts.py
@@ -0,0 +1,33 @@
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from DataBaseModel import Base, Firefighter, HonoraryMember, Vehicle, Alert
+
+engine = create_engine('sqlite:///fire_brigade.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Insert a Person in the person table
+new_firefighter = Firefighter(name='Janina', last_name='Nowak', is_active=True)
+new_firefighter2 = Firefighter(name='Jan', last_name='Nowak', is_active=True)
+new_member = HonoraryMember(name='Janko', last_name='Nowak', is_active=False)
+new_member2 = HonoraryMember(name='Janinka', last_name='Nowak', is_active=True)
+new_vehicle = Vehicle(type='GCBA', name='Ciężki', description='Gaśniczy ciężki bojowy z autopompą', seats_amount=6)
+new_alert = Alert(timestamp=datetime.datetime.now(), reason='Pożar śmietnika')
+
+new_alert.vehicles = [new_vehicle]
+new_alert.persons = [new_firefighter, new_firefighter2]
+
+session.add(new_firefighter)
+session.add(new_firefighter2)
+session.add(new_member)
+session.add(new_member2)
+session.add(new_vehicle)
+session.add(new_alert)
+session.commit()
